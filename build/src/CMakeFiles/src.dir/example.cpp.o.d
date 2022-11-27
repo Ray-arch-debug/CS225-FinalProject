9d4f459af72be50326c27293ef422fff8fe66c5c
Brendan Biernacki <bab8@illinois.edu>
2022-11-27 19:56:15 +0000

    Fixed CMakeLists and Makefile

---
+++ build/src/CMakeFiles/src.dir/example.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/src.dir/example.cpp.o: \
+  /workspaces/cs225/CS225-FinalProject/src/example.cpp \
+  /workspaces/cs225/CS225-FinalProject/src/example.h
---
+++ build/test_tests-b12d07c.cmake
@@ -0,0 +1,3 @@
+add_test( test /workspaces/cs225/CS225-FinalProject/build/test test  )
+set_tests_properties( test PROPERTIES WORKING_DIRECTORY /workspaces/cs225/CS225-FinalProject/build)
+set( test_TESTS test)
